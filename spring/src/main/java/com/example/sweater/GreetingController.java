10d5dd11ac8553cfac58e5bdfe0eb05e94d65ddc
D-DO <37467092+D-DO@users.noreply.github.com>
2018-07-19 22:27:31 +0600

    Add postgresql

--- spring/src/main/java/com/example/sweater/GreetingController.java
+++ spring/src/main/java/com/example/sweater/GreetingController.java
@@ -1,8 +1,12 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -10,12 +14,14 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
 //            Model model
-          Map<String, Object> model
+            Map<String, Object> model
 
     ) {
         model.put("name", name);
@@ -24,11 +30,36 @@ public class GreetingController {
     }
 
     @GetMapping
-    public String main(Map<String,Object> model
+    public String main(Map<String, Object> model
+    ) {
+        Iterable<Message> messages = messageRepo.findAll();
 
-    ){
-        model.put("some", "Lets code");
+        model.put("some", messages);
         return "main";
     }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model
+    ) {
+       Message message = new Message(text,tag);
+       messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+//    @PostMapping
+//    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+//        Message message = new Message(text, tag);
+//
+//        messageRepo.save(message);
+//
+//        Iterable<Message> messages = messageRepo.findAll();
+//
+//        model.put("messages", messages);
+//
+//        return "main";
+//    }
 
 }
