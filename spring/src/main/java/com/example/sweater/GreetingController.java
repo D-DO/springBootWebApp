3ca61c897003d52935bc38c44932b7d5d9b00679
D-DO <37467092+D-DO@users.noreply.github.com>
2018-07-22 22:07:52 +0600

    Add security 1

--- spring/src/main/java/com/example/sweater/GreetingController.java
+++ spring/src/main/java/com/example/sweater/GreetingController.java
@@ -18,19 +18,19 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+//            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
 //            Model model
             Map<String, Object> model
 
     ) {
-        model.put("name", name);
+//        model.put("name", name);
         //       model.addAttribute("name", name);
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model
     ) {
         Iterable<Message> messages = messageRepo.findAll();
